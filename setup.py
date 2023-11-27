44ea61d3a09c8e99fcbf3bb0e5a8627cc31d7171
lucidrains <lucidrains@gmail.com>
2023-11-27 13:19:48 -0800

    discounting was not done correctly for n-step

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'q-transformer',
   packages = find_packages(exclude=[]),
-  version = '0.0.7',
+  version = '0.0.8',
   license='MIT',
   description = 'Q-Transformer',
   author = 'Phil Wang',
